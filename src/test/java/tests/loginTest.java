6f87e8429bdea8870a393e5f48c78023e735048a
Rahul <rahulmiddha92@gmail.com>
2023-12-17 16:37:22 +0530

    updated

--- src/test/java/tests/loginTest.java
+++ src/test/java/tests/loginTest.java
@@ -2,6 +2,8 @@ package tests;
 
 
 
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.Properties;
 import org.testng.annotations.Test;
@@ -30,10 +32,17 @@ public class loginTest {
 		
 		//Load properties file
 		
-		prop.load(loginTest.class.getClassLoader().getResourceAsStream("MyProject.properties"));
-		
-		String uName=prop.getProperty("uName");//AgentHunt
-		String pwd=prop.getProperty("Pwd");//MissionImpossible
+		//prop.load(loginTest.class.getClassLoader().getResourceAsStream("MyProject.properties"));
+		FileInputStream fis = new FileInputStream("src/test/resources/MyProject.properties");
+		prop.load(fis);
+		String uName_prop=prop.getProperty("uName");//AgentHunt
+		String pwd_prop=prop.getProperty("Pwd");//MissionImpossible
+
+		String username_mvn=System.getProperty("username");
+		String password_mvn=System.getProperty("password");
+
+		String uName=(username_mvn!=null)?username_mvn:uName_prop;
+		String pwd=(password_mvn!=null)?password_mvn:pwd_prop;
 		
 		System.out.println("Username Value is ==> " +uName);
 		System.out.println("Password Value is ==> " +pwd);
